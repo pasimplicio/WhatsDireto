d1802c3f1f4b91c9a81a5d02e74f96807d36813b
pasimplicio <pasimplicio@gmail.com>
2026-03-12 08:36:39 -0300

    release v1.2.0

---
+++ app/src/main/java/br/com/whatsdireto/ui/theme/Color.kt
@@ -0,0 +1,12 @@
+package br.com.whatsdireto.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+val WhatsAppGreen = Color(0xFF25D366)
+val WhatsAppDarkGreen = Color(0xFF128C7E)
+val WhatsAppHeaderGreen = Color(0xFF075E54)
+val WhatsAppBg = Color(0xFFECE5DD)
+val ChatBubble = Color(0xFFFFFFFF)
+val DoodleLine = Color(0x1A54656F)
+val TextPrimary = Color(0xFF111B21)
+val TextSecondary = Color(0xFF667781)
